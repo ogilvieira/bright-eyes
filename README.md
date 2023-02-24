df6ab2a2892ca56e2c7a99cdc597c6ef3614a393
Gil Vieira <gil.vieira@servicenet.com.br>
2023-02-24 19:02:39 -0300

    add readme, pacotes essenciais, user-model

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+## Requerimentos
+- (Node v16)[https://nodejs.org/docs/latest-v16.x/api/]
+- Template Engine (EJS)[https://ejs.co/]
+
 ## Para iniciar
 ```
 node index.js
--- index.js
+++ index.js
@@ -1,11 +1,32 @@
+const Sequelize = require('sequelize');
 const express = require('express')
 const app = express()
 const port = 3000
 
+// VIEWS CONFIG
+app.set('view engine', 'ejs');
+app.set('view', './views/');
+app.use(express.static('public'))
+
+// Configura Models/Banco
+const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: 'banco-de-dados.sqlite'
+});
+
+// ROTAS
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+
+sequelize.authenticate((ev) => {
+    console.info(ev);
+    
+    app.listen(port, () => {
+        console.log(`Rodando em => http://localhost:${port}`)
+    })      
+    
+});
+
+
